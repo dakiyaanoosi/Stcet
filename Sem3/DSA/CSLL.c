e13b295178cf9b0eefb448248c943e3266b855e3
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2025-08-19 08:17:28 +0530

    Started CSLL

---
+++ Sem3/DSA/CSLL.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct Node *head=NULL;
+int size=0;
+
+struct Node* createNode(int value) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data=value;
+    newNode->next=NULL;
+    return newNode;
+}
+
+void insertFirst(int value) {
+    struct Node *newNode=createNode(value);
+    if(head==NULL) {
+        head=newNode;
+        newNode->next=head;
+    }
+    else {
+        struct Node *temp=head;
+        while(temp->next!=head) {
+            temp=temp->next;
+        }
+        newNode->next=head;
+        head=newNode;
+        temp->next=newNode;
+    }
+    size++;
+}
+
+void insertLast(int value) {
+    struct Node *newNode=createNode(value);
+    if(head==NULL) {
+        head=newNode;
+        newNode->next=head;
+    }
+    else {
+        struct Node *temp=head;
+        while(temp->next!=head) {
+            temp=temp->next;
+        }
+        temp->next=newNode;
+        newNode->next=head;
+    }
+    size++;
+}
+
+void printList() {
+    struct Node *temp=head;
+    for(int i=0;i<size;i++) {
+        printf("%d -->", temp->data);
+        temp=temp->next;
+    }
+    printf("\n|");
+    for(int i=0;i<size;i++) {
+        if(i==size-1) {
+            printf("___");
+            break;
+        }
+        printf("_____");
+    }
+    printf("|\n");
+}
+
+int main() {
+    insertFirst(5);
+    insertFirst(3);
+    insertFirst(1);
+    insertLast(7);
+    insertLast(9);
+    insertFirst(5);
+    insertFirst(3);
+    insertFirst(1);
+    insertLast(7);
+    insertLast(9);
+
+    printList();
+    return 0;
+}
