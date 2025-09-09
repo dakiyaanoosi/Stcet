b114aaa1cbf3a768fe8c61fc0951b03b8b9dda6e
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2025-09-09 08:32:28 +0530

    Update SLL.c

--- Sem3/DSA/SLL.c
+++ Sem3/DSA/SLL.c
@@ -6,8 +6,7 @@ struct Node {
     struct Node *next;
 };
 
-struct Node *head = NULL;
-int size = 0;
+struct Node *HEAD = NULL;
 
 struct Node* createNode(int value) {
     struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
@@ -16,74 +15,74 @@ struct Node* createNode(int value) {
     return newNode;
 }
 
+void printList() {
+    struct Node *temp=HEAD;
+    printf("List: ");
+    while(temp!=NULL) {
+        printf("%d --> ",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL\n");
+}
+
 void insertFirst(int value) {
     struct Node *newNode = createNode(value);
-    newNode->next=head;
-    head=newNode;
-    size++;
+    newNode->next=HEAD;
+    HEAD=newNode;
 }
 
 void insertLast(int value) {
-    struct Node *newNode = createNode(value);
-
-    size++;
-    if(head==NULL) {
-        head=newNode;
+    struct Node *newNode=createNode(value);
+    if(HEAD==NULL) {
+        HEAD=newNode;
         return;
     }
-
-    struct Node *temp = head;
+    struct Node *temp=HEAD;
     while(temp->next!=NULL) {
         temp=temp->next;
     }
     temp->next=newNode;
 }
 
-void insertAt(int index, int value) {
-    if(index<0 || index>size) {
-        printf("Invaid Index!\n");
+void insertAfter(int data, int value) {
+    struct Node *temp=HEAD;
+    if(temp==NULL) {
+        printf("Error: List is Empty!\n");
         return;
     }
-    if(index==0) {
-        insertFirst(value);
-        return;
+    while(temp->data!=data) {
+        temp=temp->next;
+        if(temp==NULL) {
+            printf("Error: %d NOT FOUND in the List!\n", data);
+            return;
+        }
     }
-
-    struct Node *temp = head;
-    for(int i=0; i<index-1; i++)
-    temp=temp->next;
-
-    struct Node *newNode = createNode(value);
+    struct Node *newNode=createNode(value);
     newNode->next=temp->next;
     temp->next=newNode;
-    size++;
 }
 
 void deleteFirst() {
-    if(head==NULL) {
-        printf("List is Empty!\n");
+    if(HEAD==NULL) {
+        printf("Error: List is Empty!\n");
         return;
     }
-    struct Node *temp=head;
-    head=head->next;
+    struct Node *temp=HEAD;
+    HEAD=HEAD->next;
     free(temp);
-    size--;
 }
 
 void deleteLast() {
-    if(head==NULL) {
-        printf("List is Empty!\n");
+    if(HEAD==NULL) {
+        printf("Error: List is Empty!\n");
         return;
     }
-
-    size--;
-    if(head->next==NULL) {
-        free(head);
-        head=NULL;
+    if(HEAD->next==NULL) {
+        free(HEAD);
+        HEAD=NULL;
         return;
     }
-
-    struct Node *temp=head;
+    struct Node *temp=HEAD;
     while(temp->next->next!=NULL) {
         temp=temp->next;
     }
@@ -91,61 +90,24 @@ void deleteLast() {
     temp->next=NULL;
 }
 
-void deleteAt(int index) {
-    if(index<0 || index>=size) {
-        printf("Invalid Index!!\n");
+void deleteAfter(int data) {
+    if(HEAD==NULL) {
+        printf("Error: List is Empty!\n");
         return;
     }
-    if(index==0) {
-        deleteFirst();
-        return;
-    }
-
-    struct Node *temp=head;
-    for(int i=0;i<index-1;i++) {
+    struct Node *temp=HEAD;
+    while(temp->data!=data) {
         temp=temp->next;
-    }
-    struct Node *toDelete=temp->next;
-    temp->next=temp->next->next;
-    free(toDelete);
-    size--;
-}
-
-int search(int value) {
-    int index=0;
-    struct Node *temp = head;
-    while(temp!=NULL) {
-        if(temp->data==value) {
-            return index;
+        if(temp==NULL) {
+            printf("Error: %d NOT FOUND in the List!\n", data);
+            return;
         }
-        temp=temp->next;
-        index++;
     }
-    return -1;
-}
-
-int getSize() {
-    return size;
-}
-
-void printList() {
-    struct Node *temp = head;
-    printf("List: ");
-    while(temp!=NULL) {
-        printf("%d --> ",temp->data);
-        temp=temp->next;
+    if(temp->next==NULL) {
+        printf("Error: No value after %d!\n", data);
+        return;
     }
-    printf("NULL\n");
-    printf("Size: %d\n", size);
-}
-
-void clearList() {
-    struct Node *temp=head;
-    while(temp!=NULL) {
-        struct Node *f=temp;
-        temp=temp->next;
-        free(f);
-    }
-    size=0;
-    head=NULL;
+    struct Node *temp2=temp->next;
+    temp->next=temp->next->next;
+    free(temp2);
 }
