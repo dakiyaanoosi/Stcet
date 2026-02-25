6adbcae30b327d9318cf917c26de0e1fece629ad
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2026-02-25 10:33:17 +0530

    search & delete in BST

--- Sem3/DSA/Trees/BST.c
+++ Sem3/DSA/Trees/BST.c
@@ -21,6 +21,47 @@ struct Node* insert(struct Node *ROOT, int value) {
     return ROOT;
 }
 
+int search(struct Node *ROOT, int key) {
+    if(ROOT==NULL) return 0;
+    if(ROOT->data == key) return 1;
+    if(key < ROOT->data) return search(ROOT->left, key);
+    else return search(ROOT->right, key);
+}
+
+struct Node* delete(struct Node *ROOT, int key) {
+    if(ROOT==NULL) return NULL;
+
+    if(key < ROOT->data) ROOT->left = delete(ROOT->left, key);
+
+    else if(key > ROOT->data) ROOT->right = delete(ROOT->right, key);
+
+    else {
+        if(ROOT->left == NULL && ROOT->right == NULL) {
+            free(ROOT);
+            return NULL;
+        }
+        else if(ROOT->right == NULL) {
+            struct Node *temp = ROOT->left;
+            free(ROOT);
+            return temp;
+        }
+        else if(ROOT->left==NULL) {
+            struct Node *temp = ROOT->right;
+            free(ROOT);
+            return temp;
+        }
+        else {
+            struct Node *temp = ROOT->right;
+            while(temp->left!=NULL) {
+                temp=temp->left;
+            }
+            ROOT->data = temp->data;
+            ROOT->right=delete(ROOT->right, temp->data);
+        }
+    }
+    return ROOT;
+}
+
 void inorder(struct Node *ROOT) {
     if(ROOT==NULL) return;
     inorder(ROOT->left);
@@ -37,8 +78,14 @@ int main() {
     ROOT=insert(ROOT, 1);
     ROOT=insert(ROOT, 4);
     ROOT=insert(ROOT, 6);
+    
+    inorder(ROOT);
+    printf("\n");
+
+    ROOT=delete(ROOT, 5);
 
     inorder(ROOT);
+    printf("\n");
 
     return 0;
 }
