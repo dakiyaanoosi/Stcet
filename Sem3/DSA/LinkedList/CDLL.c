d729dec4c41cc145785c38d10f0d1f188d21a953
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2026-03-31 08:49:09 +0530

    Done CDLL

--- Sem3/DSA/LinkedList/CDLL.c
+++ Sem3/DSA/LinkedList/CDLL.c
@@ -4,72 +4,94 @@
 struct Node {
     int data;
     struct Node *next;
+    struct Node *prev;
 };
 
 struct Node *createNode(int value) {
     struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data=value;
-    newNode->next=NULL;
+    newNode->next=newNode->prev=NULL;
     return newNode;
 }
 
 struct Node *TAIL=NULL;
 
-int isEmpty() {
-    return (TAIL==NULL);
-}
-
 void insertFirst(int value) {
     struct Node *newNode = createNode(value);
-    if(isEmpty()) {
+    if(TAIL==NULL) {
         TAIL=newNode;
-        newNode->next=TAIL;
+        newNode->next=newNode->prev=TAIL;
         return;
     }
 
+    TAIL->next->prev=newNode;
+    newNode->prev=TAIL;
     newNode->next=TAIL->next;
-    TAIL->next = newNode;
+    TAIL->next=newNode;
 }
 
 void insertLast(int value) {
     struct Node *newNode = createNode(value);
-    if(isEmpty()) {
+    if(TAIL==NULL) {
         TAIL=newNode;
-        newNode->next=TAIL;
+        newNode->next=newNode->prev=TAIL;
         return;
     }
 
+    TAIL->next->prev=newNode;
+    newNode->prev=TAIL;
     newNode->next=TAIL->next;
     TAIL->next=newNode;
     TAIL=newNode;
 }
 
 void insertAfter(int data, int value) {
-    if(isEmpty()) {
+    if(TAIL==NULL) {
         printf("List is Empty\n");
         return;
     }
-
-    struct Node *temp=TAIL->next;
+    struct Node *temp = TAIL->next;
     while(temp->data!=data) {
-        if(temp->next == TAIL->next) {
-            printf("%d not found in the List\n", data);
+        if(temp->next==TAIL->next) {
+            printf("%d, not found in the List\n", data);
             return;
         }
         temp=temp->next;
     }
 
     struct Node *newNode = createNode(value);
+
+    temp->next->prev=newNode;
+    newNode->prev=temp;
     newNode->next=temp->next;
     temp->next=newNode;
-    if(temp==TAIL) {
-        TAIL=newNode;
+    if(temp==TAIL) TAIL= newNode;
+}
+
+void insertbefore(int data, int value) {
+    if(TAIL==NULL) {
+        printf("List is Empty\n");
+        return;
     }
-    return;
+    struct Node *temp = TAIL->next;
+    while(temp->data!=data) {
+        if(temp->next==TAIL->next) {
+            printf("%d, not found in the List\n", data);
+            return;
+        }
+        temp=temp->next;
+    }
+
+    struct Node *newNode = createNode(value);
+
+    newNode->prev=temp->prev;
+    newNode->next=temp;
+    temp->prev->next=newNode;
+    temp->prev=newNode;
 }
 
 void deleteFirst() {
-    if(isEmpty()) {
+    if(TAIL==NULL) {
         printf("List is Empty\n");
         return;
     }
@@ -80,42 +102,39 @@ void deleteFirst() {
         return;
     }
 
-    struct Node *temp=TAIL->next;
-    TAIL->next=temp->next;
-    free(temp);
-}
-
-void deleteLast() {
-    if(isEmpty()) {
-        printf("List is Empty\n");
-        return;
-    }
-
-    if(TAIL->next==TAIL) {
-        free(TAIL);
-        TAIL=NULL;
-        return;
-    }
-
-    struct Node *temp=TAIL->next;
-    while(temp->next!=TAIL) {
-        temp=temp->next;
-    }
-    
-    struct Node *toDelete=temp->next;
-    temp->next=toDelete->next;
-    TAIL=temp;
+    struct Node *toDelete = TAIL->next;
+    toDelete->next->prev=TAIL;
+    TAIL->next=toDelete->next;
     free(toDelete);
 }
 
-void deleteAfter(int data) {
-    if(isEmpty()) {
+void deletelast() {
+    if(TAIL==NULL) {
         printf("List is Empty\n");
         return;
     }
 
     if(TAIL->next==TAIL) {
-        if(TAIL->data == data) {
+        free(TAIL);
+        TAIL=NULL;
+        return;
+    }
+
+    struct Node *toDelete = TAIL; 
+    TAIL->prev->next=TAIL->next;
+    TAIL->next->prev=TAIL->prev;
+    TAIL=TAIL->prev;
+    free(toDelete);
+}
+
+void delete(int data) {
+    if(TAIL==NULL) {
+        printf("List is Empty\n");
+        return;
+    }
+
+    if(TAIL->next==TAIL) {
+        if(TAIL->data==data) {
             free(TAIL);
             TAIL=NULL;
             return;
@@ -124,32 +143,23 @@ void deleteAfter(int data) {
         return;
     }
 
-    if(TAIL->data==data) {
-        deleteFirst();
-        return;
-    }
-
     struct Node *temp=TAIL->next;
-    while(temp!=TAIL && temp->data!=data) {
+    while(temp->data!=data) {
         temp=temp->next;
-    }
-
-    if(temp->data==data) {
-        struct Node *toDelete=temp->next;
-        if(toDelete==TAIL) {
-            deleteLast();
+        if(temp==TAIL->next) {
+            printf("%d, not found in the List\n", data);
             return;
         }
-        temp->next=toDelete->next;
-        free(toDelete);
-        return;
     }
 
-    printf("%d not found in the List\n", data);
+    temp->prev->next=temp->next;
+    temp->next->prev=temp->prev;
+    if(temp==TAIL) TAIL=temp->prev;
+    free(temp);
 }
 
 void display() {
-    if(isEmpty()) {
+    if(TAIL==NULL) {
         printf("List is Empty\n");
         return;
     }
@@ -157,8 +167,8 @@ void display() {
     struct Node *temp=TAIL->next;
     printf("List: ");
     while(temp != TAIL) {
-        printf("%d --> ", temp->data);
+        printf("%d <--> ", temp->data);
         temp=temp->next;
     }
-    printf("%d --> HEAD\n", TAIL->data);
+    printf("%d <--> HEAD\n", TAIL->data);
 }
