abc2c82813cd13abce261cc0a9136562c103fc77
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2026-04-09 06:33:15 +0000

    Added XNOR

---
+++ Sem4/CA/XNOR/testbench.sv
@@ -0,0 +1,31 @@
+module xnor_gate_tb;
+  reg a, b;
+  wire y;
+  
+  xnor_gate uut(.a(a), .b(b), .y(y));
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, xnor_gate_tb);
+    
+    a=0;
+    b=0;
+    #10;
+    
+    a=0;
+    b=1;
+    #10;
+    
+    a=1;
+    b=0;
+    #10;
+    
+    a=1;
+    b=1;
+    #10;
+    
+    a=0;
+    b=0;
+    #10;
+  end
+endmodule
