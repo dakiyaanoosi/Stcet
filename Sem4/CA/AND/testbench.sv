907477b38ec85487f37c9c626c64507a2314f163
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2026-04-09 05:24:16 +0000

    Verilog AND

---
+++ Sem4/CA/AND/testbench.sv
@@ -0,0 +1,26 @@
+module and_gate_tb();
+  reg a, b;
+  wire y;
+  and_gate uut(.a(a), .b(b), .y(y));
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars();
+    a=0;
+    b=0;
+    #10;
+    
+    a=0;
+    b=1;
+    #10;
+    
+    a=1;
+    b=0;
+    #10;
+    
+    a=1;
+    b=1;
+    #10;
+  end
+  
+endmodule
