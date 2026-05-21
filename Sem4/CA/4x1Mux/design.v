7d95576d3c00e835abd33669a69c954a03d225d6
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2026-05-21 05:49:54 +0000

    ..

--- Sem4/CA/4x1Mux/design.v
+++ Sem4/CA/4x1Mux/design.v
@@ -1,3 +1,3 @@
 module mux4x1(input [1:0]s, input [3:0]a, output f);
-	assign f= s[0]==0 ? s[1]==0 ? a[0] : a[1] : s[1]==0 ? a[2] : a[3];
+	assign f= ~s[0] ? ~s[1] ? a[0] : a[1] : s~[1] ? a[2] : a[3];
 endmodule
